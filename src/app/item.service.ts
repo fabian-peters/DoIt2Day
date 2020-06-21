a92753da1510b1256b29a73b5a311c74cc667639
Fabian Peters <cowboyfab@hotmail.de>
2020-06-21 15:50:04 +0200

    Add CRUD operations
    Signed-off-by: Fabian Peters <cowboyfab@hotmail.de>

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Item} from './item';
 
@@ -9,14 +9,35 @@ import {Item} from './item';
 export class ItemService {
 
   private itemsUrl = 'api/items';
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
   constructor(
     private http: HttpClient
   ) { }
 
-  /** GET heroes from the server */
+  // TODO add error handling and logging to server requests
+  /** GET: get items from the server */
   getItems(): Observable<Item[]> {
     return this.http.get<Item[]>(this.itemsUrl);
   }
 
+  /** PUT: update the item in the server */
+  updateItem(item: Item): Observable<any> {
+    return this.http.put(this.itemsUrl, item, this.httpOptions);
+  }
+
+  /** POST: add a new item to the server */
+  addItem(item: Item): Observable<Item> {
+    return this.http.post<Item>(this.itemsUrl, item, this.httpOptions);
+  }
+
+  /** DELETE: delete the item from the server */
+  deleteItem(item: Item | number): Observable<Item> {
+    const id = typeof item === 'number' ? item : item.id;
+    const url = `${this.itemsUrl}/${id}`;
+
+    return this.http.delete<Item>(url, this.httpOptions);
+  }
 }
